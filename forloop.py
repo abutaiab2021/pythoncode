4629654eee36dc37e6f4c57b7f95a583f9391494
abutaiab2021 <abutaiabcse95@gmail.com>
2022-06-23 12:59:44 +0600

    update2

--- forloop.py
+++ forloop.py
@@ -3,8 +3,59 @@
 #     print(i)
 
 
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-n = 100
-for i in list:
-    namta = n*i
-    print("100 x", i, "=", namta)
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# n = 100
+# for i in list:
+#     namta = n*i
+#     print("100 x", i, "=", namta)
+
+# print("\n\n\n")
+
+# list = [10, 30, 23, 43, 65, 12]
+# sum = 0
+# for i in list:
+#     sum = sum+i
+# print("The Total is:", sum)
+
+# print("\n\n\n")
+
+# for i in range(10):
+#     print(i, end=' ')
+
+
+# # namta likhar system
+# n = int(input("Enter the number :"))
+# for i in range(1, 11):
+#     c = n*i
+#     print(n, "*", i, "=", c)
+
+print("\n\n\n")
+n = int(input("Enter a number:"))
+for i in range(2, n, 2):
+    print(i)
+
+print("\n")
+list = ['Peter', 'Joseph', 'Ricky', 'Devansh']
+for i in range(len(list)):
+    print("Hello", list[i])
+
+print("\n")
+# User input for number of rows
+rows = int(input("Enter the rows:"))
+# Outer loop will print number of rows
+for i in range(0, rows+1):
+    # Inner loop will print number of Astrisk
+    for j in range(i):
+        print("*", end='')
+    print()
+
+
+print("\n")
+# User input for number of rows
+rows = int(input("Enter the rows:"))
+# Outer loop will print number of rows
+for i in range(0, rows+1):
+    # Inner loop will print number of Astrisk
+    for j in range(i):
+        print(i, end="")
+    print()
---
+++ whileloop.py
@@ -0,0 +1,45 @@
+# # prints all letters except 'a' and 't'
+# i = 0
+# str1 = 'javatpoint'
+
+# while i < len(str1):
+#     if str1[i] == 'a' or str1[i] == 't':
+#         i += 1
+#         continue
+#     print('Current Letter :', a[i])
+#     i += 1
+
+
+# # prints all letters except 'a' and 't'
+# i = 0
+# str1 = 'javatpoint'
+
+# while i < len(str1):
+#     if str1[i] == 't':
+#         i += 1
+#         break
+#     print('Current Letter :', str1[i])
+#     i += 1
+
+
+# An empty loop
+# i = 0
+# str = 'javatpoint'
+# while i < len(str):
+#     i += 1
+#     pass
+# print("total value of the word is :", i)
+
+# i = 1
+# while (i < 11):
+#     print(i)
+#     i += 1
+
+# namta print by while loop
+i = 1
+number = 0
+b = 9
+number = int(input("Please Enter a Number:"))
+while i < 10:
+    print("%d x %d=%d\n" % (number, i, number*i))
+    i += 1
